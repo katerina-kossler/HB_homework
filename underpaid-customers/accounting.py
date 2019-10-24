75499ca888a601f09b18e354f211beeb909db1c3
Katerina Kossler <katerinakossler@gmail.com>
2019-10-23 22:12:25 -0700

    start initial work on payment checking

---
+++ underpaid-customers/accounting.py
@@ -0,0 +1,38 @@
+melon_cost = 1.00
+
+customer1_name = "Joe"
+customer1_melons = 5
+customer1_paid = 5.00
+
+customer2_name = "Frank"
+customer2_melons = 6
+customer2_paid = 6.00
+
+customer3_name = "Sally"
+customer3_melons = 3
+customer3_paid = 3.00
+
+customer4_name = "Sean"
+customer4_melons = 9
+customer4_paid = 9.50
+
+customer5_name = "David"
+customer5_melons = 4
+customer5_paid = 4.00
+
+customer6_name = "Ashley"
+customer6_melons = 3
+customer6_paid = 2.00
+
+def check_payment(path):
+  order_log = open(path) # took this from the previous hw!
+  for customer in 
+  payment_expected = customer1_melons * melon_cost
+    if payment_expected_expected != customer1_paid:
+    
+    print(f"{customer1_name} paid ${customer1_paid:.2f},",
+          f"expected ${customer1_expected:.2f}"
+          )
+
+
+
